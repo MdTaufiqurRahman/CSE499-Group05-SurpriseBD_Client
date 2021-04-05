4d63948e34e990dc202853e8a8bb0368102f5192
Md. Taufiqur Rahman <56925314+MdTaufiqurRahman@users.noreply.github.com>
2021-04-05 12:59:04 +0600

    header added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
 import Header from './Component/Header/Header';
+import Home from './Component/Home/Home';
+import Login from './Component/Login/Login';
+import Footer from './Component/Footer/Footer';
+
 
 function App() {
   return (
-    <div>
+    <Router>
       <Header></Header>
-      
-    </div>
+      <Switch>
+        <Route path="/home">
+          <Home></Home>
+        </Route>
+        <Route path="/login">
+          <Login></Login>
+        </Route>
+        <Route exact path="/">
+          <Home />
+        </Route>
+      </Switch>
+      <Footer></Footer>
+    </Router>
   );
 }
 
--- src/Component/Header/Header.js
+++ src/Component/Header/Header.js
@@ -1,18 +1,33 @@
 import {Navbar, Nav} from "react-bootstrap";
 import React from "react";
+import { Link } from 'react-router-dom';
+import header from '../../images/header.png';
+import './Header.css';
 
 const Header = () => {
   return (
-
-      <Navbar bg="dark" variant="dark">
-        <Navbar.Brand href="#home">Surprise BD</Navbar.Brand>
-        <Nav className="mr-auto">
-          <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#customize">Customize Order</Nav.Link>
-          <Nav.Link href="#about">About Us</Nav.Link>
-          <Nav.Link href="#contact">Contact Us</Nav.Link>
-        </Nav>
-      </Navbar>
+    <div style={{ backgroundImage: `linear-gradient( rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5) ), url(${header})` }} className="header">
+    <nav className="nav">
+        <ul>
+            <li>
+                <Link to="/home">Home</Link>
+            </li>
+            <li>
+                <Link to="/login">Login</Link>
+            </li>
+            <li>
+                <Link to="/login">About Us</Link>
+            </li>
+            <li>
+                <Link className="btn-book" to="/book">Gift Item</Link>
+            </li>
+        </ul>
+    </nav>
+      <div className="title-container">
+          <h1>SURPRISE BD</h1>
+          <h2>SPREAD A LITTLE HAPPINESS</h2>
+      </div>
+</div>
   );
 };
 
