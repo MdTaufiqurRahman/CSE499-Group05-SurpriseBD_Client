7a2766cf0b2a83647b163e3738376a8043ca99e8
Md. Taufiqur Rahman <56925314+MdTaufiqurRahman@users.noreply.github.com>
2021-04-15 19:42:15 +0600

    adding

--- src/Component/Login/Login.js
+++ src/Component/Login/Login.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Login = () => {
     return (
         <div>
-            <h1>This is login</h1>
+            <h1 style={{ textAlign: 'center', margin:'100px' }} >This will be login</h1>
         </div>
     );
 };
