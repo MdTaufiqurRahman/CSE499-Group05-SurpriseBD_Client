5f310cfcd5af9eeabde05955027ed0252cf3c01b
unknown <muba2307@gmail.com>
2021-06-25 20:18:56 +0600

    Update Shipment.js

--- src/Component/Shipment/Shipment.js
+++ src/Component/Shipment/Shipment.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Shipment = () => {
     return (
         <div>
-            <h1 style={{border:"10px", margin:"10px", padding:"100px", textAlign:"center"}} >Shipment page under maintenance, hopefully it will update soon.</h1>
+            <h1 style={{border:"10px", margin:"10px", padding:"100px", textAlign:"center"}} >Shipment page under maintenance, hopefully it will be updated soon.</h1>
         </div>
     );
 };
