110498b01d09ccc0fccfc2f3d7d2e3a10cf5a085
Tahmid Islam <tahmid231@gmail.com>
2021-06-30 01:10:30 +0600

    Shipment page form added with design

--- src/Component/Shipment/Shipment.js
+++ src/Component/Shipment/Shipment.js
@@ -1,11 +1,80 @@
-import React from 'react';
+import React, { useContext } from "react";
+import { useForm } from "react-hook-form";
+import { UserContext } from "../../App";
+import "./Shipment.css";
 
 const Shipment = () => {
-    return (
-        <div>
-            <h1 style={{border:"10px", margin:"10px", padding:"100px", textAlign:"center"}} >Shipment page under maintenance, hopefully it will be updated soon.</h1>
-        </div>
-    );
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+  const [loggedInUser, setLoggedInUser] = useContext(UserContext);
+  const onSubmit = (data) => console.log(data);
+
+  console.log(watch("example")); // watch input value by passing the name of it
+
+  return (
+    <div className="shipment container">
+      <div className="form-container pt-5">
+        <form className="ship-form" onSubmit={handleSubmit(onSubmit)}>
+          <h4 className="text-center text-primary font-weight-bold text-uppercase">
+            Please fill the shipment form
+          </h4>
+          <input
+            defaultValue={loggedInUser.name}
+            {...register("nameRequired", { required: true })}
+            placeholder="Your Name"
+          />
+          {errors.nameRequired && (
+            <span className="error">Name is required</span>
+          )}
+
+          <input
+            defaultValue={loggedInUser.email}
+            {...register("emailRequired", { required: true })}
+            placeholder="Your Email"
+          />
+          {errors.nameRequired && (
+            <span className="error">Email is required</span>
+          )}
+
+          <input
+            {...register("addressRequired", { required: true })}
+            placeholder="Your Address"
+          />
+          {errors.nameRequired && (
+            <span className="error">Address is required</span>
+          )}
+
+          <input
+            {...register("phoneRequired", { required: true })}
+            placeholder="Your Phone"
+          />
+          {errors.nameRequired && (
+            <span className="error">Phone number is required</span>
+          )}
+
+          <input
+            {...register("descriptionRequired", { required: true })}
+            placeholder="Describe when you need this product"
+          />
+          {errors.nameRequired && (
+            <span className="error">This field is required</span>
+          )}
+
+          <input type="submit" />
+        </form>
+      </div>
+      <div className="ship-img">
+        <img
+          src="https://cdn.pixabay.com/photo/2015/11/06/12/53/roller-1027351_960_720.jpg"
+          alt=""
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Shipment;
