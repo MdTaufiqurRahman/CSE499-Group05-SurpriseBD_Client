6efa3ea4589806dfaa13ec87419dc9e2987dfc52
Tahmid Islam <kumar.pranto@northsouth.edu>
2021-08-26 21:07:35 +0600

    Customize page data sent in database done

--- src/Component/AppointmentForm/AppointmentForm.js
+++ src/Component/AppointmentForm/AppointmentForm.js
@@ -27,7 +27,19 @@ const AppointmentForm = ({ modalIsOpen, closeModal, appointmentOn, date }) => {
   const onSubmit = (data) => {
     data.service = appointmentOn;
     data.date = date;
-    data.created = new Date();
+    date.created = new Date();
+    fetch("http://localhost:5000/addAppointment", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(data),
+    })
+      .then((res) => res.json())
+      .then((success) => {
+        if (success) {
+          closeModal();
+          alert("Appointment created successfully.");
+        }
+      });
   };
 
   return (
